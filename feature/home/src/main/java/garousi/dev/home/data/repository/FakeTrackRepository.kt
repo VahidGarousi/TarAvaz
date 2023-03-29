5effb99f3629b411cf65266ea39d925326d4eaa5
Vahid Garousi <garousiv@gmail.com>
2023-03-29 17:40:00 +0430

    Implement Track.kt composable for #9

--- feature/home/src/main/java/garousi/dev/home/data/repository/FakeTrackRepository.kt
+++ feature/home/src/main/java/garousi/dev/home/data/repository/FakeTrackRepository.kt
@@ -15,29 +15,75 @@ val shadmehrAghili = Artist(
     name = "شادمهر عقیلی"
 )
 
+val fakeTracks = arrayListOf<Track>().apply {
+    add(
+        Track(
+            id = UUID.randomUUID().toString(),
+            name = "گل مریم",
+            duration = 256000,
+            releaseDate = 256000,
+            artist = rezaBahram,
+            cover = "https://tabamusic.com/wp-content/uploads/2020/11/Reza-Bahram-Gole-Maryam.jpg"
+        )
+    )
+    add(
+        Track(
+            id = UUID.randomUUID().toString(),
+            name = "قاضی",
+            duration = 256000,
+            releaseDate = 256000,
+            artist = shadmehrAghili,
+            cover = "https://www.topseda.ir/wp-content/uploads/2020/08/Shadmehr-Aghili-%E2%80%93-Ghazi.jpg"
+        )
+    )
+    add(
+        Track(
+            id = UUID.randomUUID().toString(),
+            name = "گل مریم",
+            duration = 256000,
+            releaseDate = 256000,
+            artist = rezaBahram,
+            cover = "https://tabamusic.com/wp-content/uploads/2020/11/Reza-Bahram-Gole-Maryam.jpg"
+        )
+    )
+    add(
+        Track(
+            id = UUID.randomUUID().toString(),
+            name = "قاضی",
+            duration = 256000,
+            releaseDate = 256000,
+            artist = shadmehrAghili,
+            cover = "https://www.topseda.ir/wp-content/uploads/2020/08/Shadmehr-Aghili-%E2%80%93-Ghazi.jpg"
+        )
+    )
+
+    add(
+        Track(
+            id = UUID.randomUUID().toString(),
+            name = "گل مریم",
+            duration = 256000,
+            releaseDate = 256000,
+            artist = rezaBahram,
+            cover = "https://tabamusic.com/wp-content/uploads/2020/11/Reza-Bahram-Gole-Maryam.jpg"
+        )
+    )
+    add(
+        Track(
+            id = UUID.randomUUID().toString(),
+            name = "قاضی",
+            duration = 256000,
+            releaseDate = 256000,
+            artist = shadmehrAghili,
+            cover = "https://www.topseda.ir/wp-content/uploads/2020/08/Shadmehr-Aghili-%E2%80%93-Ghazi.jpg"
+        )
+    )
+}
+
+
 class FakeTrackRepository @Inject constructor(
 
 ) : TrackRepository {
     override suspend fun getLatestTracks(): Result<List<Track>> {
-        return Result.success(arrayListOf<Track>().apply {
-            add(
-                Track(
-                    id = UUID.randomUUID().toString(),
-                    name = "گل مریم",
-                    duration = 256000,
-                    releaseDate = 256000,
-                    artist = rezaBahram
-                )
-            )
-            add(
-                Track(
-                    id = UUID.randomUUID().toString(),
-                    name = "دور شدی",
-                    duration = 256000,
-                    releaseDate = 256000,
-                    artist = shadmehrAghili
-                )
-            )
-        })
+        return Result.success(fakeTracks)
     }
 }
--- feature/home/src/main/java/garousi/dev/home/presentation/Track.kt
+++ feature/home/src/main/java/garousi/dev/home/presentation/Track.kt
@@ -1,39 +1,89 @@
 package garousi.dev.home.presentation
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import garousi.dev.design_system.debugPlaceholder
+import garousi.dev.design_system.theme.TarAvazTheme
+import garousi.dev.home.R
+import garousi.dev.home.domain.model.Artist
 import garousi.dev.home.domain.model.Track
+import java.util.UUID
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TrackCard(
+fun Track(
     track: Track,
     onClick: (Track) -> Unit
 ) {
-    Column {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .height(160.dp)
+            .padding(end = 12.dp)
+    ) {
         Card(
             onClick = { onClick(track) },
             colors = CardDefaults.elevatedCardColors(
                 containerColor = Color.DarkGray
-            )
+            ),
+            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
         ) {
-            Box(
-                modifier = Modifier
-                    .size(96.dp)
-                    .padding(end = 12.dp)
+            AsyncImage(
+                model = track.cover,
+                contentDescription = track.cover,
+                placeholder = debugPlaceholder(R.drawable.gole_maryam),
+                modifier = Modifier.size(115.dp)
             )
         }
-        Text(text = track.name)
-        Text(text = track.artist.name)
+        Text(
+            text = track.name,
+            color = Color(0xDEFFFFFF),
+            style = MaterialTheme.typography.labelLarge
+        )
+        Text(
+            text = track.artist.name,
+            color = Color(0x99FFFFFF),
+            style = MaterialTheme.typography.labelMedium
+        )
+    }
+}
+
+
+@Composable
+@Preview
+fun TrackPreview() {
+    val rezaBahram = Artist(
+        id = UUID.randomUUID().toString(),
+        name = "رضا بهرام"
+    )
+    val track = Track(
+        id = UUID.randomUUID().toString(),
+        name = "گل مریم",
+        duration = 256000,
+        releaseDate = 256000,
+        artist = rezaBahram,
+        cover = "http://tabamusic.com/wp-content/uploads/2020/11/Reza-Bahram-Gole-Maryam.jpg"
+    )
+    TarAvazTheme {
+        CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+            Track(track = track, onClick = {})
+        }
     }
 }
