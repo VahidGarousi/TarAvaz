3d3d3b25faaf78963ee6a52a3f1c633ef951ec37
Vahid Garousi <garousiv@gmail.com>
2025-03-24 15:57:56 +0330

    #13 Add file-level suppressions for `LongParameterList` and `MagicNumber` in design system theme files.

--- core/design-system/src/main/java/ir/taravaz/core/designsystem/theme/TarAvazLightTokens.kt
+++ core/design-system/src/main/java/ir/taravaz/core/designsystem/theme/TarAvazLightTokens.kt
@@ -1,3 +1,5 @@
+@file:Suppress("MagicNumber")
+
 package ir.taravaz.core.designsystem.theme
 
 import androidx.compose.ui.graphics.Color
--- core/design-system/src/main/java/ir/taravaz/core/designsystem/theme/TaravazColors.kt
+++ core/design-system/src/main/java/ir/taravaz/core/designsystem/theme/TaravazColors.kt
@@ -1,3 +1,5 @@
+@file:Suppress("LongParameterList")
+
 package ir.taravaz.core.designsystem.theme
 
 import androidx.compose.runtime.Immutable
