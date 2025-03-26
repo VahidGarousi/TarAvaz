88370b2b10cacdd9d04cd3432fe780ce5e815706
Vahid Garousi <garousiv@gmail.com>
2025-03-26 14:15:22 +0330

    #18 Adding playlist module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,6 +29,7 @@ include(":app")
 include(":core:domain", ":core:screenshot-testing", ":core:design-system", ":core:ui")
 include(":feature:player:data", ":feature:player:presentation", ":feature:player:domain")
 include(":feature:home:presentation")
+include(":feature:playlist:presentation")
 
 check(JavaVersion.current().isCompatibleWith(JavaVersion.VERSION_17)) {
     """
