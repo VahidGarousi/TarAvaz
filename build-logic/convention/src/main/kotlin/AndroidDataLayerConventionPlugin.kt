1cca26e9907f1e9f4a3daaef755e77d6e6c0cab5
Vahid Garousi <garousiv@gmail.com>
2025-03-28 14:27:37 +0330

    #29 Add `core:domain` as a dependency to the data layer.

--- build-logic/convention/src/main/kotlin/AndroidDataLayerConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidDataLayerConventionPlugin.kt
@@ -1,4 +1,3 @@
-import ir.taravaz.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.dependencies
@@ -6,6 +5,7 @@ import org.gradle.kotlin.dsl.dependencies
 class AndroidDataLayerConventionPlugin : Plugin<Project> {
     override fun apply(project: Project) = project.run {
         dependencies {
+            add("implementation", project(":core:domain"))
         }
     }
 }
