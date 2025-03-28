03d383c310bc3f87f1bf42372ae3cabcd8db6faf
Vahid Garousi <garousiv@gmail.com>
2025-03-28 14:10:56 +0330

    #29 Add use cases tests
    - Implemented `GetLatestPlayablesUseCase` to retrieve the latest playable items.
    - Implemented `GetPlayableBannersUseCase` to fetch playable banners.
    - Created `TrackRepository` interface to abstract data fetching logic.
    - Added comprehensive unit tests for `GetLatestPlayablesUseCase` and `GetPlayableBannersUseCase`.
    - Tests cover various scenarios including empty data, single/multiple items, exceptions, network issues, large datasets, concurrency, edge cases, and duplicates.
    - Updated the structure of the `GetLatestPlayablesUseCase` and the `GetPlayableBannersUseCase` to get the data from `TrackRepository` interface.

--- feature/home/domain/src/main/java/ir/taravaz/home/domain/GetLatestPlayablesUseCase.kt
+++ feature/home/domain/src/main/java/ir/taravaz/home/domain/GetLatestPlayablesUseCase.kt
@@ -1,9 +1,12 @@
 package ir.taravaz.home.domain
 
 import ir.taravaz.core.domain.model.Playables
+import ir.taravaz.home.domain.repository.TrackRepository
 
-class GetLatestPlayablesUseCase {
-    operator fun invoke(): Playables {
-        TODO("")
+class GetLatestPlayablesUseCase(
+    private val repository: TrackRepository,
+) {
+    suspend operator fun invoke(): Playables {
+        return repository.getLatestPlayables()
     }
 }
